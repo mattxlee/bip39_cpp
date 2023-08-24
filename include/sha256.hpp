ad6489ed6c16fd2dee071e13d204bebff0a81246
Matthew Lee <mattxlee@gmail.com>
2023-08-25 00:30:49 +0800

    Improve sha256 by using upgraded methods.

--- include/sha256.hpp
+++ include/sha256.hpp
@@ -7,39 +7,59 @@
 
 #include <stdexcept>
 
-#include <openssl/sha.h>
+#include <openssl/evp.h>
 
 namespace bip39 {
 
 class SHA256 {
 public:
     SHA256()
+        : mdctx_(EVP_MD_CTX_new())
     {
-        if (!SHA256_Init(&ctx_)) {
-            throw std::runtime_error("cannot initialize sha256 context");
+        if (mdctx_ == nullptr) {
+            throw std::runtime_error("create new md context object is failed");
+        }
+        if (1 != EVP_DigestInit_ex(mdctx_, EVP_sha256(), nullptr)) {
+            throw std::runtime_error("cannot initialize md context object");
         }
     }
 
-    void Update(uint8_t const* data, int n)
+    ~SHA256()
     {
-        if (!SHA256_Update(&ctx_, data, n)) {
-            throw std::runtime_error("cannot update data to sha256");
+        if (mdctx_) {
+            EVP_MD_CTX_free(mdctx_);
+        }
+    }
+
+    void Update(void const* data, int n)
+    {
+        if (1 != EVP_DigestUpdate(mdctx_, data, n)) {
+            throw std::runtime_error("cannot update data to md context object");
         }
     }
 
     std::vector<uint8_t> const& GetResult()
     {
         if (res_.empty()) {
-            res_.resize(SHA256_DIGEST_LENGTH);
-            if (!SHA256_Final(res_.data(), &ctx_)) {
-                throw std::runtime_error("cannot get sha256 digest");
+            int md_size = EVP_MD_size(EVP_sha256());
+            uint8_t* pmd_out = reinterpret_cast<uint8_t*>(OPENSSL_malloc(md_size));
+            if (nullptr == pmd_out) {
+                throw std::runtime_error("failed to allocate memory");
             }
+            uint32_t len;
+            if (1 != EVP_DigestFinal_ex(mdctx_, pmd_out, &len)) {
+                throw std::runtime_error("cannot calculate sha256 digest");
+            }
+            assert(len == md_size);
+            res_.resize(md_size);
+            memcpy(res_.data(), pmd_out, md_size);
+            OPENSSL_free(pmd_out);
         }
         return res_;
     }
 
 private:
-    SHA256_CTX ctx_;
+    EVP_MD_CTX* mdctx_{nullptr};
     std::vector<uint8_t> res_;
 };
 
