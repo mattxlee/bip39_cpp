613e4c142783277901db56914b20ffdcb3f81d11
Matthew Lee <mattxlee@gmail.com>
2023-08-25 00:30:48 +0800

    Add random bytes generator.

--- include/basic_defs.h
+++ include/basic_defs.h
@@ -7,6 +7,10 @@ namespace bip39 {
 
 std::string const LANGUAGES[] = {"chinese_simplified", "chinese_traditional", "czech", "english", "french", "italian", "japanese", "korean", "portuguese", "spanish"};
 
+// the number of bits of check-sum
+int const CS_MIN = 4;
+int const CS_MAX = 8;
+
 } // namespace bip39
 
 #endif
--- src/test.cpp
+++ src/test.cpp
@@ -2,6 +2,7 @@
 
 #include "basic_defs.h"
 #include "words.hpp"
+#include "random.hpp"
 
 TEST(WordList, Initial)
 {
@@ -9,4 +10,14 @@ TEST(WordList, Initial)
     for (auto const& lang : bip39::LANGUAGES) {
         EXPECT_EQ(word_list.Load(lang).size(), 2048);
     }
+}
+
+TEST(Random, Random)
+{
+    bip39::RandomBytes bytes(128);
+    auto res = bytes.Random();
+    EXPECT_EQ(res.size(), 128);
+
+    auto res2 = bytes.Random();
+    EXPECT_NE(res, res2);
 }
