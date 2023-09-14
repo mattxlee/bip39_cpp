8a5a3ffb5f869512172b497b2f4c6ec264a75b27
Matthew Lee <mattxlee@gmail.com>
2023-09-15 03:07:31 +0800

    Build mntool with cxxopts.

--- src/mnemonic-toolbox/main.cpp
+++ src/mnemonic-toolbox/main.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
 
+#include <cxxopts.hpp>
+
 int main(int argc, char *argv[]) {
+    cxxopts::Options opts("Mnemonic tool", "A tool to generate mnemonic passphrase");
+    opts.add_options()
+        ("h,help", "Show help")
+        ;
+    auto result = opts.parse(argc, argv);
+    if (result.count("help") > 0) {
+        std::cout << opts.help() << std::endl;
+        return 0;
+    }
     return 0;
 }
